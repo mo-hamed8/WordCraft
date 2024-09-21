0b295f9d98536a231c9d260361cb196e7b16698d
Mohammed <mohamme5hame5@gmail.com>
2024-09-22 00:24:30 +0300

    Define columns in migration files

--- database/migrations/2024_09_21_211332_create_sm2s_table.php
+++ database/migrations/2024_09_21_211332_create_sm2s_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Word;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('sm2s', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Word::class)->onDelete("cascade");
+            $table->integer('repetitions')->default(0);
+            $table->double('ef')->default(2.5);
+            $table->integer('interval')->default(1);
+            $table->date('next_review_date');
+            $table->date('last_review_date');
             $table->timestamps();
         });
     }
