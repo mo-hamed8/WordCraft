681cecd86fe9030e3a6274d9c8fdda4343fcc958
Mohammed <mohamme5hame5@gmail.com>
2024-09-22 03:11:45 +0300

    Implement logic for adding new words

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WordController extends Controller
 {
@@ -11,7 +12,50 @@ class WordController extends Controller
         return view("Word.create");
     }
 
-    public function store(){
-        return "you are in store function";
+    public function store(Request $request){
+        $user=Auth::user();
+
+        $request->validate([
+            "text"=>"required"
+        ]);
+        $words=$this->processWords($request->text);
+
+        $wordsWithCount = array_count_values($words);
+
+        foreach($wordsWithCount as $key=>$value){
+            $word=$user->words()->where("word",$key)->first();
+            if($word){
+                $word->repetition=$word->repetition+$value;
+                $word->save();
+            }
+            else{
+                $user->words()->create(["word"=>$key,"repetition"=>$value]);
+            }
+        }
+
+
+        // Remove duplicate words
+        $words = array_unique($words);
+
+        return redirect(route("word.create"));
+    }
+
+    private function processWords($text): array {
+        // Convert the text to lowercase for consistency
+        $text = strtolower($text);
+    
+        // Remove punctuation and special characters using a regular expression
+        $text = preg_replace("/[^\w\s]/u", " ", $text);
+    
+        // Split the text into an array of words based on whitespace
+        $words = preg_split("/\s+/", $text);
+
+
+        // Delete empty items
+        $words = array_filter($words);
+    
+        // Return the array of words
+        return $words;
+    
     }
 }
